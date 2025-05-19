d96c9654dc2285908c6aae14e67eacdf9a273a69
Pedro Fernandez <Pedro.Fernandez@beyond-hospitality.com>
2025-05-19 17:10:09 +0200

    feat(application): implement validator person command

--- src/Travelport.Application/Validators/CreatePersonValidator.cs
+++ src/Travelport.Application/Validators/CreatePersonValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Travelport.Application.DTOs;
+using Travelport.Domain.Constants;
 
 namespace Travelport.Application.Validators;
 
@@ -7,6 +8,18 @@ public class CreatePersonValidator : AbstractValidator<CreatePersonCommand>
 {
     public CreatePersonValidator()
     {
-        // TODO: Define the rules for the properties
+        RuleFor(x => x.Name).NotEmpty();
+
+        RuleFor(x => x.Surname).NotEmpty();
+
+        RuleFor(x => x.PassportNumber)
+            .NotEmpty()
+            .Matches("^[PL]{1}[A-Z]{1}[0-9]{7}$")
+            .WithMessage("Passport must start with P or L, followed by a letter and 7 digits.");
+
+        RuleFor(x => x.AirportCode)
+            .NotEmpty()
+            .Must(Airports.IsValid)
+            .WithMessage("Invalid airport code.");
     }
 }
